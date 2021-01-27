6583d2d79e08bd2f2d58e23fb4b9e957f5e4360d
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2021-01-26 19:06:47 -0600

    feat: Add welcome fragment

--- app/src/main/java/com/example/shoestoreinventory/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/shoestoreinventory/ui/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.shoestoreinventory.R
 import com.example.shoestoreinventory.databinding.FragmentLoginBinding
 
@@ -22,5 +23,15 @@ class LoginFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_WelcomeFragment)
+        }
+        binding.buttonRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_WelcomeFragment)
+        }
     }
 }
---
+++ app/src/main/java/com/example/shoestoreinventory/ui/welcome/WelcomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.shoestoreinventory.ui.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.shoestoreinventory.R
+import com.example.shoestoreinventory.databinding.FragmentWelcomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonContinue.setOnClickListener {
+            // TODO action to instructions screen
+        }
+    }
+}
