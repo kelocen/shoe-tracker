352ac6027bf58eff77a120a856d6deaf9198f9a1
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2021-01-26 20:20:39 -0600

    feat: Add instructions fragment

--- app/src/main/java/com/example/shoestoreinventory/ui/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/shoestoreinventory/ui/instructions/InstructionsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.shoestoreinventory.ui.welcome
+package com.example.shoestoreinventory.ui.instructions
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,28 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.shoestoreinventory.R
-import com.example.shoestoreinventory.databinding.FragmentWelcomeBinding
+import com.example.shoestoreinventory.databinding.FragmentInstructionsBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class WelcomeFragment : Fragment() {
-    private lateinit var binding: FragmentWelcomeBinding
+class InstructionsFragment : Fragment() {
+    private lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonContinue.setOnClickListener {
-            findNavController().navigate(R.id.action_WelcomeFragment_to_InstructionsFragment)
+            // TODO actions
         }
     }
 }
