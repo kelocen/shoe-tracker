e39c388413da5566b84528945ab1f6cb21008b77
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2021-01-27 23:58:44 -0600

    feat: Add listing fragment

--- app/src/main/java/com/example/shoestoreinventory/MainActivity.kt
+++ app/src/main/java/com/example/shoestoreinventory/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.shoestoreinventory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.shoestoreinventory.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+        setSupportActionBar(binding.toolbar)
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container_main)
+                        as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/example/shoestoreinventory/ui/listing/ListingFragment.kt
+++ app/src/main/java/com/example/shoestoreinventory/ui/listing/ListingFragment.kt
@@ -1,4 +1,4 @@
-package com.example.shoestoreinventory.ui.instructions
+package com.example.shoestoreinventory.ui.listing
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,29 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.shoestoreinventory.R
-import com.example.shoestoreinventory.databinding.FragmentInstructionsBinding
+import com.example.shoestoreinventory.databinding.FragmentListingBinding
 
 /**
  * A simple [Fragment] subclass.
  */
-class InstructionsFragment : Fragment() {
-
-    private lateinit var binding: FragmentInstructionsBinding
+class ListingFragment : Fragment() {
+    private lateinit var binding: FragmentListingBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_listing, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonContinue.setOnClickListener {
-            findNavController().navigate(R.id.action_InstructionsFragment_to_ListingFragment)
+        binding.buttonAddShoes.setOnClickListener {
+            // TODO actions
         }
     }
 }
