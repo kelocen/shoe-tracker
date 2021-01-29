75af2c7c9ebaf61dbacbc788571e93a25c9a0df3
Kenneth Cole <70181196+coleken@users.noreply.github.com>
2021-01-28 21:58:59 -0600

    feat: Add support for up button

--- app/src/main/java/com/example/shoestoreinventory/MainActivity.kt
+++ app/src/main/java/com/example/shoestoreinventory/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.shoestoreinventory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.shoestoreinventory.databinding.ActivityMainBinding
 import timber.log.Timber
@@ -25,5 +27,10 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment_container_main)
+        return navController.navigateUp(appBarConfiguration)
     }
 }
